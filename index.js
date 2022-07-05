d5eacf5c141e2b2e4ff157bbbbb4b8c62247b8bb
MakrFrost <frostmark@ukr.net>
2022-07-05 15:50:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-const dead = "Вы хотите?";
-const deadDead = "Вы хотите?";
-const deadDeadDed = "Вы хотите?";
+// const dead = "Вы хотите?";
+// const deadDead = "Вы хотите?";
+// const deadDeadDed = "Вы хотите?";
+// alert(dead);
+// confirm(deadDead);
+// const result = prompt(deadDeadDed);
+// console.log(result);
 
-alert(dead);
-confirm(deadDead);
+// console.log(0.14 + 0.88);
+// console.log((0.14 + 0.88).toFixed(99));
 
-const result = prompt(deadDeadDed);
+//! Рандомное числецо в консольку
+// const random = Math.random() * (100 - 1) + 1;
+// console.log(random.toFixed(0));
 
-console.log(result);
+//! Рандомное
+// const nameClient = [
+//   "Жмышенко Валерий",
+//   "Антон Палыч Фекалисов",
+//   "Кувшик Набиев",
+//   "Жмых Палаевв",
+//   "Ушат Голизоев",
+//   "Рулон Обоев",
+//   "Грилзат Ашанов",
+//   "Варшат Варусов",
+//   "Атобе Октоберфестов",
+//   "Люсинда Арестовна",
+// ];
+// const randomNameClient = Math.floor(Math.random() * nameClient.length);
+// const random = Math.random() * (228 - 1);
+// const randomNumber = random.toFixed(0);
+// const piece = [
+//   "отдельная геройская снайперская брыгада",
+//   " жмыховская взводная ячейка генерала констЖмыхова",
+//   "iменi Володимира Зеленського",
+//   "отдельный полк тактического назначения `Черноголовка`",
+//   "имени Люськи Арестовны",
+//   "228 полк енотиков полоскунов",
+// ];
+// const randomPiece = Math.floor(Math.random() * piece.length);
+// const text = `Добро пожаловать ${nameClient[randomNameClient]}! Вы из ${randomNumber} гвардейской ${piece[randomPiece]} армии заглощенковых?`;
+
+// console.log(text.toUpperCase());
