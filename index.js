1a82654e254a43b6fcd700e76524c660b228e536
MakrFrost <frostmark@ukr.net>
2022-07-05 14:25:52 +0300

    add files

---
+++ index.js
@@ -0,0 +1,10 @@
+const dead = "Вы хотите?";
+const deadDead = "Вы хотите?";
+const deadDeadDed = "Вы хотите?";
+
+alert(dead);
+confirm(deadDead);
+
+const result = prompt(deadDeadDed);
+
+console.log(result);
