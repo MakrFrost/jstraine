a014e05908ef9917904d73fc80184a3c10ce2bd8
MakrFrost <frostmark@ukr.net>
2022-07-20 22:52:59 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,46 +1,46 @@
-let cells = [...document.querySelectorAll(".cell")];
-cells.sort(() => Math.random() - 1);
+// let cells = [...document.querySelectorAll(".cell")];
+// cells.sort(() => Math.random() - 1);
 
-let emptyX = 3;
-let emptyY = 3;
+// let emptyX = 3;
+// let emptyY = 3;
 
-let x = 0;
-let y = 0;
+// let x = 0;
+// let y = 0;
 
-for (let cell of cells) {
-  cell.x = x;
-  cell.y = y;
+// for (let cell of cells) {
+//   cell.x = x;
+//   cell.y = y;
 
-  toEmptyPlace(cell);
+//   toEmptyPlace(cell);
 
-  x += 1;
+//   x += 1;
 
-  if (x > 3) {
-    y += 1;
-    x = 0;
-  }
+//   if (x > 3) {
+//     y += 1;
+//     x = 0;
+//   }
 
-  cell.onclick = () => {
-    let dX = Math.abs(cell.x - emptyX);
-    let dY = Math.abs(cell.y - emptyY);
-    let distance = dX + dY;
+//   cell.onclick = () => {
+//     let dX = Math.abs(cell.x - emptyX);
+//     let dY = Math.abs(cell.y - emptyY);
+//     let distance = dX + dY;
 
-    if (distance < 2) {
-      toEmptyPlace(cell);
-    }
-  };
-}
+//     if (distance < 2) {
+//       toEmptyPlace(cell);
+//     }
+//   };
+// }
 
-function toEmptyPlace(cell) {
-  const newX = emptyX;
-  const newY = emptyY;
+// function toEmptyPlace(cell) {
+//   const newX = emptyX;
+//   const newY = emptyY;
 
-  emptyX = cell.x;
-  emptyY = cell.y;
+//   emptyX = cell.x;
+//   emptyY = cell.y;
 
-  cell.x = newX;
-  cell.y = newY;
+//   cell.x = newX;
+//   cell.y = newY;
 
-  cell.style.left = `${newX * 25}%`;
-  cell.style.top = `${newY * 25}%`;
-}
+//   cell.style.left = `${newX * 25}%`;
+//   cell.style.top = `${newY * 25}%`;
+// }
