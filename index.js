0e4fb712c109f2f897e102916c7b2704f9eacf77
MakrFrost <frostmark@ukr.net>
2022-08-07 23:48:47 +0300

    dom update

--- index.js
+++ index.js
@@ -784,26 +784,38 @@
 // console.log(image.src); // https://placeimg.com/640/480/animals
 // image.src = "https://placeimg.com/640/480/tech";
 
-const text = document.querySelector("#paragraph");
+// const text = document.querySelector("#paragraph");
 
-console.log(text.classList); // ["text", "red", "big", value: "text red big"]
+// console.log(text.classList); // ["text", "red", "big", value: "text red big"]
 
-console.log(text.classList.contains("red")); // true
+// console.log(text.classList.contains("red")); // true
 
-text.classList.remove("big");
-console.log(text.classList); // ["text", "red", value: "text red"]
+// text.classList.remove("big");
+// console.log(text.classList); // ["text", "red", value: "text red"]
 
-text.classList.add("new-class");
-console.log(text.classList); // ["text", "red", "new-class", value: "text red new-class"]
+// text.classList.add("new-class");
+// console.log(text.classList); // ["text", "red", "new-class", value: "text red new-class"]
 
-// Can add multiple classes
-text.classList.add("a", "b", "c");
-console.log(text.classList);
+// // Can add multiple classes
+// text.classList.add("a", "b", "c");
+// console.log(text.classList);
 
-text.classList.toggle("is-hidden"); // will add is-hidden class
-text.classList.toggle("is-hidden"); // will remove is-hidden class
+// text.classList.toggle("is-hidden"); // will add is-hidden class
+// text.classList.toggle("is-hidden"); // will remove is-hidden class
 
-// classList has a forEach method
-text.classList.forEach((cls) => {
-  console.log(cls); // text, red, new-class
-});
+// // classList has a forEach method
+// text.classList.forEach((cls) => {
+//   console.log(cls); // text, red, new-class
+// });
+
+const image = document.querySelector(".image");
+
+console.log(image.attributes); // NamedNodeMap {0: class, 1: src, 2: alt, length: 3}
+
+console.log(image.hasAttribute("src")); // true
+
+console.log(image.getAttribute("alt")); // "Lake and clouds"
+
+image.setAttribute("alt", "Amazing nature");
+
+console.log(image.getAttribute("alt")); // Amazing nature
