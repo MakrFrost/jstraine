8a4b13e03b1ffcfd74e72d3290b7b5f0ed40facb
MakrFrost <frostmark@ukr.net>
2022-08-07 23:31:02 +0300

    Дом упдэйт

--- index.js
+++ index.js
@@ -753,20 +753,57 @@
 // Math.floor(Math.random() * 100 + 15);
 // Math.floor(Math.random() * 100 + 8);
 
-const listWithId = document.querySelector("#menu");
-listWithId.style.textTransform = "uppercase";
-listWithId.style.fontSize = "24px";
-console.log(listWithId);
+// const listWithId = document.querySelector("#menu");
+// listWithId.style.textTransform = "uppercase";
+// listWithId.style.fontSize = "24px";
+// console.log(listWithId);
 
-const listWithClass = document.querySelector(".menu");
-console.log(listWithClass);
+// const listWithClass = document.querySelector(".menu");
+// console.log(listWithClass);
 
-const menuItemsByTagName = document.querySelectorAll("li");
-console.log(menuItemsByTagName);
+// const menuItemsByTagName = document.querySelectorAll("li");
+// console.log(menuItemsByTagName);
 
-const menuItemsByClass = document.querySelectorAll(".menu-item");
-console.log(menuItemsByClass);
+// const menuItemsByClass = document.querySelectorAll(".menu-item");
+// console.log(menuItemsByClass);
 
-const firstMenuItem = document.querySelector(".menu-item");
-firstMenuItem.style.color = "tomato";
-console.log(firstMenuItem);
+// const firstMenuItem = document.querySelector(".menu-item");
+// firstMenuItem.style.color = "tomato";
+// console.log(firstMenuItem);
+
+// //===================================================
+// const message = document.querySelector("#message");
+// console.log(message.value); // Default textarea message
+
+// //===================================================
+// const activeLink = document.querySelector(".btn.active");
+// console.log(activeLink.href); // https://s.codepen.io/about
+
+// //===================================================
+// const image = document.querySelector(".image");
+// console.log(image.src); // https://placeimg.com/640/480/animals
+// image.src = "https://placeimg.com/640/480/tech";
+
+const text = document.querySelector("#paragraph");
+
+console.log(text.classList); // ["text", "red", "big", value: "text red big"]
+
+console.log(text.classList.contains("red")); // true
+
+text.classList.remove("big");
+console.log(text.classList); // ["text", "red", value: "text red"]
+
+text.classList.add("new-class");
+console.log(text.classList); // ["text", "red", "new-class", value: "text red new-class"]
+
+// Can add multiple classes
+text.classList.add("a", "b", "c");
+console.log(text.classList);
+
+text.classList.toggle("is-hidden"); // will add is-hidden class
+text.classList.toggle("is-hidden"); // will remove is-hidden class
+
+// classList has a forEach method
+text.classList.forEach((cls) => {
+  console.log(cls); // text, red, new-class
+});
